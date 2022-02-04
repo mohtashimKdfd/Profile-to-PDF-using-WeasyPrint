6af25336e6094eab2d2dd1accf2c9f66cd6ddbd2
mohtashimKdfd <mohtashim.kamran@unthinkable.co>
2022-02-04 16:07:53 +0530

    changes

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_weasyprint import HTML, render_pdf
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
@@ -18,11 +19,13 @@ class User(db.Model):
     bio = db.Column(db.String,nullable=True)
     hobby = db.Column(db.String,nullable=True)
     address = db.Column(db.String,nullable=True)
+    image = db.Column(db.Text,nullable=True)
 
-    def __init__(self,username,password,email):
+    def __init__(self,username,password,email,image):
         self.username=username
         self.email=email
         self.password=password
+        self.image=image
 
 class UserSerializer(marsh.Schema):
     class Meta:
@@ -43,11 +46,12 @@ def signup():
         email = request.json['email']
         password = request.json['password']
         hashed_password = generate_password_hash(password)
+        image = request.json['image']
 
         if User.query.filter_by(username=username).count():
             return jsonify({'msg':'User already registered'})
         else:
-            newUser = User(username=username, email=email, password=hashed_password)
+            newUser = User(username=username, email=email, password=hashed_password,image=image)
             try:
                 db.session.add(newUser)
                 db.session.commit()
@@ -64,12 +68,34 @@ def login():
         if User.query.filter_by(email=email).count():
             targetUser = User.query.filter_by(email=email).first()
             if check_password_hash(targetUser.password,password)==True:
-                return render_template('profile.html',user=targetUser)
+                html = render_template('profile.html',user=targetUser)
+                # return html   
+                return render_pdf(HTML(string=html))
             else:
                 return jsonify({'msg':'Wrong password'})
         else:
             return jsonify({'msg':'No user found'})
     else:
         return render_template('login.html')
+
+@app.route('/update/<int:id>', methods=['POST','GET'])
+def update(id):
+    TargetUser = User.query.filter_by(id=id).first()
+    if TargetUser:
+        if request.method == 'POST':
+            bio = request.form['bio']
+            address = request.form['address']
+            hobby = request.form['hobby']
+            TargetUser.bio = bio
+            TargetUser.address = address
+            TargetUser.hobby = hobby
+            db.session.commit()
+            html = render_template('profile.html',user=TargetUser)
+            return render_pdf(HTML(string=html))
+        elif request.method == 'GET':
+            return render_template('update.html',user=TargetUser)
+    else: 
+        return jsonify({"status":"No user found"})
+
 if __name__ == '__main__':
     app.run(debug=True)
